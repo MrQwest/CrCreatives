e7e7160895e908c33d7640070ff51ec45e6bd356
Phil Wareham <phil@designhive.com>
2012-04-26 11:57:30 +0100

    Adjust format in CSS files for consistency and to cure my OCD
    Also, roll map CSS into main Less file

--- index.php
+++ index.php
@@ -23,7 +23,6 @@
 	<meta name="viewport" content="width=device-width, target-densitydpi=160dpi, initial-scale=1">
 	
 	<link rel="stylesheet" href="css/style.css" media="all">
-	<link rel="stylesheet" href="css/map.css" media="all">
 	
 	<!-- For iPhone 4 -->
 	<link rel="apple-touch-icon-precomposed" sizes="114x114" href="img/h/apple-touch-icon.png">
