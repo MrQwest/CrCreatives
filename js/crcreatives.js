7cd7acb3f3c7b18c072592585c7f069dc961bc65
Jack Franklin <jack@jackfranklin.net>
2012-04-27 15:14:13 +0100

    Tidied up the JS code
    within the on(click) you can use $(this) to get at #missyellow rather
    than reselect a#missyellow.
    
    Within the fadeOut callback this points to the item faded out so you can
    use this rather than reselect
    
    Swapped out .click for .on("click")

--- js/crcreatives.js
+++ js/crcreatives.js
@@ -2,9 +2,11 @@ $(function() {
 
 		$('.yellowswitch').html('<a id="missyellow" href="#" title="Miss the yellow?">Miss the yellow?</a>');
 		
-		$("#missyellow").click(function(event) {
+		$("#missyellow").on("click", function(event) {
 			event.preventDefault();
-			$('div.anystretch').fadeOut('fast', function(){ $('div.anystretch').remove(); });
-			$('a#missyellow').css("color", "#000");
+			$('div.anystretch').fadeOut('fast', function() {
+        $(this).remove();
+      });
+			$(this).css("color", "#000");
 		});
 });
