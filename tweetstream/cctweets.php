aff077192b284924412bd806de4d797f64289798
Phil Wareham <phil@designhive.com>
2012-04-27 10:30:30 +0100

    It's impolite to open a new window for links

--- tweetstream/cctweets.php
+++ tweetstream/cctweets.php
@@ -21,8 +21,8 @@
     $text= preg_replace("/(^|[\n ])([\w]*?)((ht|f)tp(s)?:\/\/[\w]+[^ \,\"\n\r\t<]*)/is", "$1$2<a href=\"$3\" >$3</a>", $text);
     $text= preg_replace("/(^|[\n ])([\w]*?)((www|ftp)\.[^ \,\"\t\n\r<]*)/is", "$1$2<a href=\"http://$3\" >$3</a>", $text);
     $text= preg_replace("/(^|[\n ])([a-z0-9&\-_\.]+?)@([\w\-]+\.([\w\-\.]+)+)/i", "$1<a href=\"mailto:$2@$3\">$2@$3</a>", $text);
-    $text= preg_replace("/@(\w+)/", '<a href="http://www.twitter.com/$1" target="_blank">@$1</a>', $text);  
-    $text= preg_replace("/\#(\w+)/", '<a href="http://search.twitter.com/search?q=$1" target="_blank">#$1</a>',$text);  
+    $text= preg_replace("/@(\w+)/", '<a href="http://www.twitter.com/$1" rel="external">@$1</a>', $text);  
+    $text= preg_replace("/\#(\w+)/", '<a href="http://search.twitter.com/search?q=$1" rel="external">#$1</a>',$text);  
     return($text);
 }
 
