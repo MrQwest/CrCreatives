74cdb6f0f42a798dd60ef4ef61bc5572d65e5e0b
Jack Franklin <jack@jackfranklin.net>
2012-04-27 15:11:53 +0100

    added event to the function call for the click handler so preventDefault actually works

--- js/crcreatives.js
+++ js/crcreatives.js
@@ -1,8 +1,8 @@
-$(document).ready(function(){ // let's go!
+$(function() {
 
 		$('.yellowswitch').html('<a id="missyellow" href="#" title="Miss the yellow?">Miss the yellow?</a>');
 		
-		$("#missyellow").click(function() {
+		$("#missyellow").click(function(event) {
 			event.preventDefault();
 			$('div.anystretch').fadeOut('fast', function(){ $('div.anystretch').remove(); });
 			$('a#missyellow').css("color", "#000");
