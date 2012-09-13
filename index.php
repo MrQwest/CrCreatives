1805c9e2ddc735e03a16f43dec25c9b26e2af989
Anthony Killeen <mrqwest@gmail.com>
2012-09-13 08:34:30 +0100

    updated index to reflect Septembers date

--- index.php
+++ index.php
@@ -60,7 +60,7 @@ $attendees = array('crcreatives', 'mrqwest', 'simianstudios', 'alexjsexton', 'si
 	</header>
 	
 	<section id="next">
-		<h1>Next: <time datetime="2012-07-25T19:00:00Z"><a href="http://lanyrd.com/series/croydoncreatives/save-to-calendar/">29th August @ 7pm</a></time></h1>
+		<h1>Next: <time datetime="2012-09-26T19:00:00Z"><a href="http://lanyrd.com/series/croydoncreatives/save-to-calendar/">26th September @ 7pm</a></time></h1>
 	</section>
 	
 	<section id="about">
