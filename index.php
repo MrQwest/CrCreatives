88b58774e5e48259b33f10126d7ab3fee81ae08a
Phil Wareham <phil@designhive.com>
2013-01-28 11:11:14 +0000

    Time tag was incorrect

--- index.php
+++ index.php
@@ -61,7 +61,7 @@ $attendees = array('crcreatives', 'mrqwest', 'simianstudios', 'alexjsexton', 'si
 	</header>
 	
 	<section id="next">
-		<h1>Next: <time datetime="2013-12-30T19:00:00Z"><a href="http://lanyrd.com/series/croydoncreatives/save-to-calendar/">30th Jan @ 7pm</a></time></h1>
+		<h1>Next: <time datetime="2013-01-30T19:00:00Z"><a href="http://lanyrd.com/series/croydoncreatives/save-to-calendar/">30th Jan @ 7pm</a></time></h1>
 	</section>
 	
 	<section id="about">
