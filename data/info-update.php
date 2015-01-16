35a4e738feae5411101ba7d14cb9b6976713585e
Mike Street <mike@liquidlight.co.uk>
2015-01-16 17:57:54 +0000

    Update tab spacing

--- data/info-update.php
+++ data/info-update.php
@@ -1,24 +1,24 @@
 <?php
 
 	function check_url($url) {
-	   $headers = @get_headers($url);
-	   $headers = (is_array($headers)) ? implode( "\n ", $headers) : $headers;
+		 $headers = @get_headers($url);
+		 $headers = (is_array($headers)) ? implode( "\n ", $headers) : $headers;
 
-	   return (bool)preg_match('#^HTTP/.*\s+[(200|301|302)]+\s#i', $headers);
+		 return (bool)preg_match('#^HTTP/.*\s+[(200|301|302)]+\s#i', $headers);
 	}
 
-  $nextDate = strtotime('Last wednesday of this month');
+	$nextDate = strtotime('Last wednesday of this month');
 
-  // time as a string for <time> element.
-  $timeNextDate = date("c", strtotime('7pm'));
+	// time as a string for <time> element.
+	$timeNextDate = date("c", strtotime('7pm'));
 
-  // date as human readable, 29th Oct
-  $showNextDate = date("jS M",$nextDate);
-  $attendingDate = date("My", $nextDate);
+	// date as human readable, 29th Oct
+	$showNextDate = date("jS M",$nextDate);
+	$attendingDate = date("My", $nextDate);
 
-  $attendlink = "http://attending.io/events/cc-".$attendingDate;
+	$attendlink = "http://attending.io/events/cc-".$attendingDate;
 
-  $link = (check_url($attendlink)) ? $attendlink : false;
+	$link = (check_url($attendlink)) ? $attendlink : false;
 
 	$data = array(
 		'showNextDate' => $showNextDate,
