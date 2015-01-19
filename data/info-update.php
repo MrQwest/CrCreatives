074ac1c25fb5b7c30b86f785e813946015ece0eb
Mike Street <mike@liquidlight.co.uk>
2015-01-19 13:04:49 +0000

    Add comments and check date status
    Info date now checks what month we are in and if the meetup for
    this month has passed. if it has, show the next one.
    
    Example data.json output:
    
    {
    	showNextDate: "28th Jan",
    	timeNextDate: "2015-01-19T19:00:00+00:00",
    	attendingDate: "Jan15",
    	link: "http://attending.io/events/cc-Jan15"
    }

--- data/info-update.php
+++ data/info-update.php
@@ -1,13 +1,18 @@
 <?php
-
+	
+	// Check_URL function checks if a URL is valid
 	function check_url($url) {
 		 $headers = @get_headers($url);
 		 $headers = (is_array($headers)) ? implode( "\n ", $headers) : $headers;
-
 		 return (bool)preg_match('#^HTTP/.*\s+[(200|301|302)]+\s#i', $headers);
 	}
 
+	// Get timestamp
 	$nextDate = strtotime('Last wednesday of this month');
+
+	// If it's still the same month, but past the date of this months meet, get the next months meet
+	if(($nextDate < time()) && (date('m') == date('m', $nextDate)))
+		$nextDate = strtotime('Last wednesday of next month');
 
 	// time as a string for <time> element.
 	$timeNextDate = date("c", strtotime('7pm'));
@@ -16,6 +21,7 @@
 	$showNextDate = date("jS M",$nextDate);
 	$attendingDate = date("My", $nextDate);
 
+	// Constrcut attending link
 	$attendlink = "http://attending.io/events/cc-".$attendingDate;
 
 	$link = (check_url($attendlink)) ? $attendlink : false;
@@ -26,6 +32,5 @@
 		'attendingDate' => $attendingDate,
 		'link' => $link
 	);
-
 
 	file_put_contents('data.json', json_encode($data));
